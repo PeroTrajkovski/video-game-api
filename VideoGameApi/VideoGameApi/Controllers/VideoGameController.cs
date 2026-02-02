01749d3f40728b3300455b04a7289aaf2dcca926
PeroTrajkovski <ptrajkovski98@gmail.com>
2026-02-02 14:43:56 +0100

    Added create, update and delete methods

--- VideoGameApi/VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/VideoGameApi/Controllers/VideoGameController.cs
@@ -58,5 +58,42 @@ namespace VideoGameApi.Controllers
             }
             return Ok(game);
         }
+        [HttpPost]
+        public ActionResult<VideoGame> AddVideoGame(VideoGame newGame)
+        {
+            if(newGame == null)
+            {
+                return BadRequest();
+            }
+            newGame.Id = videoGame.Max(vg => vg.Id) + 1;
+            videoGame.Add(newGame);
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateVideoGame(int id, VideoGame updateVideoGame)
+        {
+            var game = videoGame.FirstOrDefault(vg => vg.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            game.Title = updateVideoGame.Title;
+            game.Platform = updateVideoGame.Platform;
+            game.Developer = updateVideoGame.Developer;
+            game.Publisher = updateVideoGame.Publisher;
+
+           return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVideoGame(int id)
+        {
+            var game = videoGame.FirstOrDefault(vg => vg.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            videoGame.Remove(game);
+            return Ok($"{game.Title} is deleted successfully");
+        }
     }
 }
