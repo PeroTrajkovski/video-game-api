de5c44cdeb5d2d200db7dcce2a80b2c42c2599b1
PeroTrajkovski <ptrajkovski98@gmail.com>
2026-02-02 20:30:13 +0100

    Added database and seeded data

---
+++ VideoGameApi/VideoGameApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Models;
+
+namespace VideoGameApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<VideoGame> VideoGames { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<VideoGame>().HasData(
+                new VideoGame
+                {
+                    Id = 1,
+                    Title = "The Legend of Zelda: Breath of the Wild",
+                    Platform = "Nintendo Switch",
+                    Developer = "Nintendo",
+                    Publisher = "Nintendo"
+                },
+                new VideoGame
+                {
+                    Id = 2,
+                    Title = "God of War",
+                    Platform = "PS4",
+                    Developer = "Santa Monica Studio",
+                    Publisher = "Sony Interactive Entertainment"
+                },
+                new VideoGame
+                {
+                    Id = 3,
+                    Title = "Red Dead Redemption 2",
+                    Platform = "Multiple",
+                    Developer = "Rockstar Games",
+                    Publisher = "Rockstar Games"
+                },
+                new VideoGame
+                {
+                    Id = 4,
+                    Title = "Spider-Man 2",
+                    Platform = "PS5",
+                    Developer = "Insomniac Games",
+                    Publisher = "Sony Interactive Entertainment",
+                 }
+            );
+        }
+    }
+}
